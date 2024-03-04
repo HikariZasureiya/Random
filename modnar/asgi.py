35c8db42b57ee66777214822a25d19be89c22ca9
HikariZasureiya <info.d.podder@gmail.com>
2024-03-04 20:04:25 +0530

    hope

--- modnar/asgi.py
+++ modnar/asgi.py
@@ -12,8 +12,9 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import room.routing  # Import your routing configuration
-
+import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modnar.settings')
+django.setup()
 
 django_asgi_app = get_asgi_application()
 
